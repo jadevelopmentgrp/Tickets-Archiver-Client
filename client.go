558fd52746dc64bd6ba87d61d7fd1a700a1b1020
rxdn <29165304+rxdn@users.noreply.github.com>
2022-03-26 16:34:14 +0000

    Port to logarchiver v2 format

--- client.go
+++ client.go
@@ -6,8 +6,13 @@ import (
 	"errors"
 	"fmt"
 	"github.com/TicketsBot/common/encryption"
-	"github.com/TicketsBot/logarchiver/discord"
+	"github.com/TicketsBot/logarchiver/model"
+	v1 "github.com/TicketsBot/logarchiver/model/v1"
+	v2 "github.com/TicketsBot/logarchiver/model/v2"
+	"github.com/rxdn/gdl/objects/channel"
 	"github.com/rxdn/gdl/objects/channel/message"
+	"github.com/rxdn/gdl/objects/guild"
+	"github.com/rxdn/gdl/objects/user"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -20,7 +25,10 @@ type ArchiverClient struct {
 	key        []byte
 }
 
-var ErrExpired = errors.New("log has expired")
+var (
+	ErrExpired  = errors.New("log has expired")
+	ErrNotFound = errors.New("Transcript not found")
+)
 
 func NewArchiverClient(endpoint string, encryptionKey []byte) ArchiverClient {
 	return NewArchiverClientWithTimeout(endpoint, time.Second*3, encryptionKey)
@@ -41,54 +49,67 @@ func NewArchiverClientWithTimeout(endpoint string, timeout time.Duration, encryp
 	}
 }
 
-func (c *ArchiverClient) Get(guildId uint64, ticketId int) ([]message.Message, error) {
+func (c *ArchiverClient) Get(guildId uint64, ticketId int) (v2.Transcript, error) {
 	endpoint := fmt.Sprintf("%s/?guild=%d&id=%d", c.endpoint, guildId, ticketId)
 	res, err := c.httpClient.Get(endpoint)
 	if err != nil {
-		return nil, err
+		return v2.Transcript{}, err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		return v2.Transcript{}, err
 	}
 
 	if res.StatusCode != 200 {
 		if res.StatusCode == 404 {
-			return nil, ErrExpired
+			return v2.Transcript{}, ErrNotFound
 		}
 
 		var decoded map[string]string
 		if err := json.Unmarshal(body, &decoded); err != nil {
-			return nil, err
+			return v2.Transcript{}, err
 		}
 
-		return nil, errors.New(decoded["message"])
-	} else {
-		body, err = encryption.Decompress(body)
-		if err != nil {
-			return nil, err
-		}
+		return v2.Transcript{}, errors.New(decoded["message"])
+	}
 
-		body, err = encryption.Decrypt(c.key, body)
-		if err != nil {
-			return nil, err
-		}
+	body, err = encryption.Decompress(body)
+	if err != nil {
+		return v2.Transcript{}, err
+	}
 
+	body, err = encryption.Decrypt(c.key, body)
+	if err != nil {
+		return v2.Transcript{}, err
+	}
+
+	version := model.GetVersion(body)
+	switch version {
+	case model.V1:
 		var messages []message.Message
 		if err := json.Unmarshal(body, &messages); err != nil {
-			return nil, err
+			return v2.Transcript{}, err
 		}
 
-		return messages, nil
+		return v1.ConvertToV2(messages), nil
+	case model.V2:
+		var transcript v2.Transcript
+		if err := json.Unmarshal(body, &transcript); err != nil {
+			return v2.Transcript{}, err
+		}
+
+		return transcript, nil
+	default:
+		return v2.Transcript{}, fmt.Errorf("Unknown version %d", version)
 	}
 }
 
 func (c *ArchiverClient) Store(messages []message.Message, guildId uint64, ticketId int, premium bool) error {
-	reduced := discord.ReduceMessages(messages)
+	transcript := v2.NewTranscript(messages, v2.NoopRetriever[user.User], v2.NoopRetriever[channel.Channel], v2.NoopRetriever[guild.Role])
 
-	data, err := json.Marshal(reduced)
+	data, err := json.Marshal(transcript)
 	if err != nil {
 		return err
 	}
@@ -121,36 +142,6 @@ func (c *ArchiverClient) Store(messages []message.Message, guildId uint64, ticke
 	}
 
 	return nil
-}
-
-func (c *ArchiverClient) Encode(messages []message.Message, title string) ([]byte, error) {
-	encoded, err := json.Marshal(messages)
-	if err != nil {
-		return nil, err
-	}
-
-	endpoint := fmt.Sprintf("%s/encode?title=%s", c.endpoint, title)
-	res, err := c.httpClient.Post(endpoint, "application/json", bytes.NewReader(encoded))
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		var decoded map[string]string
-		if err := json.NewDecoder(res.Body).Decode(&decoded); err != nil {
-			return nil, err
-		}
-
-		return nil, errors.New(decoded["message"])
-	} else {
-		var buff bytes.Buffer
-		if _, err := buff.ReadFrom(res.Body); err != nil {
-			return nil, err
-		}
-
-		return buff.Bytes(), nil
-	}
 }
 
 func (c *ArchiverClient) PurgeGuild(guildId uint64) error {
