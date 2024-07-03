6696394e19b08ac219e04995669be3cd45a16d6a
rxdn <29165304+rxdn@users.noreply.github.com>
2024-07-03 17:58:01 +0100

    Refactor!

--- client.go
+++ client.go
@@ -1,82 +1,38 @@
 package archiverclient
 
 import (
-	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/TicketsBot/common/encryption"
-	"github.com/TicketsBot/logarchiver/model"
-	v1 "github.com/TicketsBot/logarchiver/model/v1"
-	v2 "github.com/TicketsBot/logarchiver/model/v2"
+	"github.com/TicketsBot/logarchiver/pkg/model"
+	v1 "github.com/TicketsBot/logarchiver/pkg/model/v1"
+	v2 "github.com/TicketsBot/logarchiver/pkg/model/v2"
 	"github.com/rxdn/gdl/objects/channel"
 	"github.com/rxdn/gdl/objects/channel/message"
 	"github.com/rxdn/gdl/objects/guild"
 	"github.com/rxdn/gdl/objects/user"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"time"
 )
 
 type ArchiverClient struct {
-	endpoint   string
-	httpClient *http.Client
-	key        []byte
+	retriever Retriever
+	key       []byte
 }
 
-var (
-	ErrExpired  = errors.New("log has expired")
-	ErrNotFound = errors.New("Transcript not found")
-)
+var ErrNotFound = errors.New("Transcript not found")
 
-type ErrorResponse struct {
-	Message string `json:"message"`
-}
-
-func NewArchiverClient(endpoint string, encryptionKey []byte) ArchiverClient {
-	return NewArchiverClientWithTimeout(endpoint, time.Second*3, encryptionKey)
-}
-
-func NewArchiverClientWithTimeout(endpoint string, timeout time.Duration, encryptionKey []byte) ArchiverClient {
-	endpoint = strings.TrimSuffix(endpoint, "/")
-
-	return ArchiverClient{
-		endpoint: endpoint,
-		httpClient: &http.Client{
-			Timeout: timeout,
-			Transport: &http.Transport{
-				TLSHandshakeTimeout: time.Second * 3,
-			},
-		},
-		key: encryptionKey,
+func NewArchiverClient(retriever Retriever, encryptionKey []byte) *ArchiverClient {
+	return &ArchiverClient{
+		retriever: retriever,
+		key:       encryptionKey,
 	}
 }
 
-func (c *ArchiverClient) Get(guildId uint64, ticketId int) (v2.Transcript, error) {
-	endpoint := fmt.Sprintf("%s/?guild=%d&id=%d", c.endpoint, guildId, ticketId)
-	res, err := c.httpClient.Get(endpoint)
+func (c *ArchiverClient) Get(ctx context.Context, guildId uint64, ticketId int) (v2.Transcript, error) {
+	body, err := c.retriever.GetTicket(ctx, guildId, ticketId)
 	if err != nil {
 		return v2.Transcript{}, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return v2.Transcript{}, err
-	}
-
-	if res.StatusCode != 200 {
-		if res.StatusCode == 404 {
-			return v2.Transcript{}, ErrNotFound
-		}
-
-		var decoded map[string]string
-		if err := json.Unmarshal(body, &decoded); err != nil {
-			return v2.Transcript{}, err
-		}
-
-		return v2.Transcript{}, errors.New(decoded["message"])
 	}
 
 	body, err = encryption.Decompress(body)
@@ -110,7 +66,7 @@ func (c *ArchiverClient) Get(guildId uint64, ticketId int) (v2.Transcript, error
 	}
 }
 
-func (c *ArchiverClient) Store(messages []message.Message, guildId uint64, ticketId int, premium bool) error {
+func (c *ArchiverClient) Store(ctx context.Context, guildId uint64, ticketId int, messages []message.Message) error {
 	transcript := v2.NewTranscript(messages, v2.NoopRetriever[user.User], v2.NoopRetriever[channel.Channel], v2.NoopRetriever[guild.Role])
 
 	data, err := json.Marshal(transcript)
@@ -125,102 +81,5 @@ func (c *ArchiverClient) Store(messages []message.Message, guildId uint64, ticke
 
 	data = encryption.Compress(data)
 
-	endpoint := fmt.Sprintf("%s/?guild=%d&id=%d", c.endpoint, guildId, ticketId)
-	if premium {
-		endpoint += "&premium"
-	}
-
-	res, err := c.httpClient.Post(endpoint, "application/json", bytes.NewReader(data))
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		var decoded map[string]string
-		if err := json.NewDecoder(res.Body).Decode(&decoded); err != nil {
-			return err
-		}
-
-		return errors.New(decoded["message"])
-	}
-
-	return nil
-}
-
-func (c *ArchiverClient) PurgeGuild(guildId uint64) error {
-	endpoint := fmt.Sprintf("%s/guild/%d", c.endpoint, guildId)
-
-	req, err := http.NewRequest(http.MethodDelete, endpoint, nil)
-	if err != nil {
-		return err
-	}
-
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusAccepted {
-		var decoded map[string]string
-		if err := json.NewDecoder(res.Body).Decode(&decoded); err != nil {
-			return err
-		}
-
-		return errors.New(decoded["message"])
-	}
-
-	return nil
-}
-
-type Status string
-
-const (
-	StatusInProgress Status = "in_progress"
-	StatusComplete   Status = "complete"
-	StatusFailed     Status = "failed"
-)
-
-type PurgeStatus struct {
-	Status  Status            `json:"status"`
-	Removed []string          `json:"removed"`
-	Failed  []string          `json:"failed"`
-	Errors  map[string]string `json:"errors"`
-}
-
-var ErrOperationNotFound = errors.New("operation not found")
-
-func (c *ArchiverClient) PurgeStatus(guildId uint64) (PurgeStatus, error) {
-	endpoint := fmt.Sprintf("%s/guild/status/%d", c.endpoint, guildId)
-
-	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
-	if err != nil {
-		return PurgeStatus{}, err
-	}
-
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return PurgeStatus{}, err
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode == 404 {
-		return PurgeStatus{}, ErrOperationNotFound
-	} else if res.StatusCode != 200 {
-		var response ErrorResponse
-		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
-			return PurgeStatus{}, err
-		}
-
-		return PurgeStatus{}, errors.New(response.Message)
-	}
-
-	var status PurgeStatus
-	if err := json.NewDecoder(res.Body).Decode(&status); err != nil {
-		return PurgeStatus{}, err
-	}
-
-	return status, nil
+	return c.retriever.StoreTicket(ctx, guildId, ticketId, data)
 }
--- go.mod
+++ go.mod
@@ -1,17 +1,19 @@
 module github.com/TicketsBot/archiverclient
 
-go 1.18
+go 1.21
+
+toolchain go1.22.4
 
 require (
 	github.com/TicketsBot/common v0.0.0-20240613013221-1e27eb8bfe37
-	github.com/TicketsBot/logarchiver v0.0.0-20220326162808-cdf0310f5e1c
+	github.com/TicketsBot/logarchiver v0.0.0-20240703162551-f7485e035ee5
 	github.com/rxdn/gdl v0.0.0-20240612163900-621eccf40179
 )
 
 require (
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
